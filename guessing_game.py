fac4fc59cb300d9e8e8733ca1d04e6524bbca799
nseitler <hello@nickseitler.com>
2023-10-11 11:41:07 -0400

    Added print_stats function and updated start_game function

--- guessing_game.py
+++ guessing_game.py
@@ -5,17 +5,60 @@ Project 1 - A Number Guessing Game
 """
 
 # Import the random and statistics modules.
+import random
+from statistics import mean, median, mode
+
+# Create print_stats function to display stats to player after completing the game
+def print_stats(list):
+	print(f"""
+		all scores = {list}
+		number of games played = {len(list)}
+		mean = {mean(list)}
+		median = {median(list)}
+		mode = {mode(list)}""")
 
 # Create the start_game function.
+def start_game():
+
+#   1. Display an intro/welcome message to the player.
+	print("Hello! Welcome to the number guessing game.")
+	
+#   2. Store a random number as the answer/solution.
+	answer = random.randint(1, 100)
+	scores = []
+	tries = 0
+
+
+#   3. Continuously prompt the player for a guess.
+	while True:
+		guess = input("Guess a number between 1 and 100:   ")
+		try:
+			guess = int(guess)
+			if guess < 1 or guess > 100:
+			print("Your guess must be a number between 1 and 100.")
+		except ValueError as err:
+			print("please choose a number.")
+
+		if guess == answer:
+			print("You guessed the right number!")
+			break
+#     a. If the guess is greater than the solution, display to the player "It's lower".
+		elif guess < answer:
+			print("It's low.")
+			continue
+#     b. If the guess is less than the solution, display to the player "It's higher".
+		elif guess > answer:
+			print("It's high.")
+			continue
+
+
+
 # Write your code inside this function.
 
 #   When the program starts, we want to:
 #   ------------------------------------
-#   1. Display an intro/welcome message to the player.
-#   2. Store a random number as the answer/solution.
-#   3. Continuously prompt the player for a guess.
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
+
+
 
 #   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
 #   5. Display the following data to the player
